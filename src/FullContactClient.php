b3e608b07ff09b31f2d526872d0955f206b05015
Dmitry Borzyonok <dima.matters@gmail.com>
2018-01-04 13:38:38 +0200

    Protect fields of client

--- src/FullContactClient.php
+++ src/FullContactClient.php
@@ -15,14 +15,14 @@ use McMatters\FullContactApi\Resources\PersonResource;
 class FullContactClient
 {
     /**
-     * @var CompanyResource
+     * @var string
      */
-    public $company;
+    protected $apiKey;
 
     /**
-     * @var PersonResource
+     * @var array
      */
-    public $person;
+    protected $resources = [];
 
     /**
      * FullContactClient constructor.
@@ -31,7 +31,37 @@ class FullContactClient
      */
     public function __construct(string $apiKey)
     {
-        $this->company = new CompanyResource($apiKey);
-        $this->person = new PersonResource($apiKey);
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @return CompanyResource
+     */
+    public function company(): CompanyResource
+    {
+        return $this->getResource('company', CompanyResource::class);
+    }
+
+    /**
+     * @return PersonResource
+     */
+    public function person(): PersonResource
+    {
+        return $this->getResource('person', PersonResource::class);
+    }
+
+    /**
+     * @param string $resource
+     * @param string $class
+     *
+     * @return mixed
+     */
+    protected function getResource(string $resource, string $class)
+    {
+        if (!isset($this->resources[$resource])) {
+            $this->resources[$resource] = new $class($this->apiKey);
+        }
+
+        return $this->resources[$resource];
     }
 }
